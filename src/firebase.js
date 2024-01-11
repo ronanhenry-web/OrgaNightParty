7ba1e4eeee7df4af67021277ba9b422bb22e2b91
Ronan HENRY <ronan.henry@supdevinci-edu.fr>
2024-01-11 23:18:40 +0100

    first commit feat: setup archi

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection } from 'firebase/firestore'
+
+export const firebaseApp = initializeApp({
+    apiKey: "AIzaSyBbvEL-Z1kA5wFNz8zxVBBQimhaZIUh5M4",
+    authDomain: "nightparty-cc2f9.firebaseapp.com",
+    projectId: "nightparty-cc2f9",
+    storageBucket: "nightparty-cc2f9.appspot.com",
+    messagingSenderId: "743712538204",
+    appId: "1:743712538204:web:aa8b0f9e0708738ddd7099",
+    measurementId: "G-ZMZNWTNCQE"
+});
+
+export const db = getFirestore(firebaseApp)
+
+export const todosRef = collection(db, 'todos')
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import { VueFire, VueFireAuth } from 'vuefire'
+import App from './App.vue'
+
+import { firebaseApp } from './firebase'
+
+const app = createApp(App)
+app.use(VueFire, {
+    firebaseApp,
+    modules: [
+        VueFireAuth(),
+    ],
+})
+
+app.mount('#app')
