75f87c5263841df433e6a59c7839b92d604bdb17
Ronan HENRY <ronan.henry@supdevinci-edu.fr>
2024-01-13 01:21:24 +0100

    feat: display all data in users

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,57 @@
 <template>
   <div>
     <h1>Liste des Personnes</h1>
-    <ul>
-      <li v-for="user in users" :key="user.id">
-        {{ user.name }} {{ user.lastname }}
-      </li>
-    </ul>
+    <el-table :data="users" style="width: 100%">
+      <el-table-column prop="name" label="Prénom" width="150" :formatter="formatFirstLetterToUpperCase" />
+      <el-table-column prop="lastname" label="Nom" width="150" :formatter="formatUppercase" />
+      <el-table-column prop="age" label="Âge" width="80" />
+      <el-table-column prop="feedback" label="Feedback" width="200" :formatter="formatFirstLetterToUpperCase" />
+      <el-table-column prop="gender" label="Genre" width="80" :formatter="formatUppercase" />
+      <el-table-column prop="paid" label="Payé" width="100" :formatter="formatBoolean" />
+      <el-table-column prop="present" label="Présent" width="100" :formatter="formatBoolean" />
+      <el-table-column prop="team.id" label="Équipe" width="150" :formatter="formatFirstLetterToUpperCase" />
+      <el-table-column prop="vegan" label="Végan" width="100" :formatter="formatBoolean" />
+      <el-table-column fixed="right" label="Opérations" width="180">
+        <template #default="{row}">
+          <el-button type="primary" size="small" @click="handleDetail(row)">Détail</el-button>
+          <el-button type="success" size="small" @click="handleEdit(row)">Éditer</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref, onMounted } from 'vue';
 import { db } from '@/firebase';
 import { collection, getDocs } from 'firebase/firestore';
+import { ElTable, ElTableColumn, ElButton  } from 'element-plus';
 
-export default {
-  name: 'HelloWorld',
-  setup() {
-    const users = ref([]);
+const users = ref([]);
 
-    onMounted(async () => {
-      const querySnapshot = await getDocs(collection(db, 'users'));
-      users.value = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    });
+onMounted(async () => {
+  const querySnapshot = await getDocs(collection(db, 'users'));
+  users.value = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+});
 
-    return { users };
-  }
-}
+const formatFirstLetterToUpperCase = (row, column, cellValue) => {
+  if (!cellValue) return '';
+  return cellValue.charAt(0).toUpperCase() + cellValue.slice(1).toLowerCase();
+};
+
+const formatUppercase = (row, column, cellValue) => {
+  return cellValue.toUpperCase();
+};
+
+const formatBoolean = (row, column, value) => {
+  return value ? 'Oui' : 'Non';
+};
+
+const handleDetail = (user) => {
+  console.log('Détails pour:', user);
+};
+
+const handleEdit = (user) => {
+  console.log('Éditer:', user);
+};
 </script>
-
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import { VueFire, VueFireAuth } from 'vuefire'
 import App from './App.vue'
 
 import { firebaseApp } from './firebase'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
+app.use(ElementPlus)
 app.use(VueFire, {
     firebaseApp,
     modules: [
